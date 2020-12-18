178c6c355270fa17cebff0d0778c5e80cbe2c6f0
MarkTiukov <tyukov.mark@gmail.com>
2020-12-19 01:22:45 +0300

    Added main functions for Parser

--- Parser.py
+++ Parser.py
@@ -15,20 +15,49 @@ class Situation:
         return self.rule.end[self.dot_position]
 
 
-class Parser:
-    def __init__(self, grammar: Grammar, word: str) -> None:
-        super().__init__()
-        self.situations = [set()] * (len(word) + 1)
-        starting_rule = grammar.rules[0]
-        self.situations[0].add(Situation(starting_rule, 0, 0))
-        current_size = -1
-        while current_size != len(self.situations[0]):
-            current_size = len(self.situations[0])
-            self.predict(0, grammar)
-            # complete(0)
+situations = list()
 
-    def predict(self, situation_number: int, grammar: Grammar):
-        for situation in self.situations[situation_number]:
-            start_symbol = situation.getCurrentSymbol()
-            for rule in grammar.getRule(start_symbol):
-                self.situations[situation_number].add(Situation(rule, 0, situation_number))
+
+def generateSituations(grammar: Grammar, word: str) -> None:
+    global situations
+    situations = [set()] * (len(word) + 1)
+    starting_rule = grammar.rules[0]
+    situations[0].add(Situation(starting_rule, 0, 0))
+    current_size = -1
+    while current_size != len(situations[0]):
+        current_size = len(situations[0])
+        predict(0, grammar)
+        complete(0)
+    for i in range(1, len(word) + 1):
+        scan(i - 1, word[i - 1])
+        current_size = -1
+        while current_size != len(situations[i]):
+            current_size = len(situations[i])
+            predict(i, grammar)
+            complete(i)
+
+
+def predict(situation_number: int, grammar: Grammar):
+    global situations
+    for situation in situations[situation_number]:
+        start_symbol = situation.getCurrentSymbol()
+        for rule in grammar.getRule(start_symbol):
+            situations[situation_number].add(Situation(rule, 0, situation_number))
+
+
+def complete(situation_number: int):
+    global situations
+    for situation in situations[situation_number]:
+        if situation.isCompleted():
+            for possible_situation in situations[situation.begin_position]:
+                if possible_situation.getCurrentSymbol() == situation.rule.begin:
+                    situations[situation_number].add(Situation(possible_situation.rule,
+                                                               possible_situation.dot_position + 1,
+                                                               possible_situation.begin_position))
+
+
+def scan(situation_number: int, symbol: str):
+    global situations
+    for situation in situations[situation_number]:
+        if situation == situation.getCurrentSymbol():
+            situations[situation_number + 1].add(Situation(situation.rule, situation.dot_position + 1, situation.begin_position))
