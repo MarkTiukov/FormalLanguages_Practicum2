db96ad418c57fe0057f5a59624ad13abce51fef4
MarkTiukov <tyukov.mark@gmail.com>
2020-12-18 22:06:16 +0300

    addded predict method for Parser class

--- Parser.py
+++ Parser.py
@@ -1,4 +1,4 @@
-from Grammar import Rule
+from Grammar import Rule, Grammar
 
 
 class Situation:
@@ -13,3 +13,22 @@ class Situation:
 
     def getCurrentSymbol(self) -> str:
         return self.rule.end[self.dot_position]
+
+
+class Parser:
+    def __init__(self, grammar: Grammar, word: str) -> None:
+        super().__init__()
+        self.situations = [set()] * (len(word) + 1)
+        starting_rule = grammar.rules[0]
+        self.situations[0].add(Situation(starting_rule, 0, 0))
+        current_size = -1
+        while current_size != len(self.situations[0]):
+            current_size = len(self.situations[0])
+            self.predict(0, grammar)
+            # complete(0)
+
+    def predict(self, situation_number: int, grammar: Grammar):
+        for situation in self.situations[situation_number]:
+            start_symbol = situation.getCurrentSymbol()
+            for rule in grammar.getRule(start_symbol):
+                self.situations[situation_number].add(Situation(rule, 0, situation_number))
